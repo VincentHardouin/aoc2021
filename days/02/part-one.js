0bd38d09d5627a3a0fb3e8a87f28994cc58ca0af
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-02 07:28:55 +0100

    day02: add part one

--- days/02/part-one.js
+++ days/02/part-one.js
@@ -1,7 +1,53 @@
 const { getInput } = require('./input');
 
-function getResult(input = getInput()) {}
+class Position {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  addForward(units) {
+    this.x += units;
+  }
+
+  addDown(units) {
+    this.y += units;
+  }
+
+  addUp(units) {
+    this.y -= units;
+  }
+
+  addInstruction({ instruction, units }) {
+    switch (instruction) {
+      case 'forward':
+        this.addForward(units);
+        break;
+      case 'down':
+        this.addDown(units);
+        break;
+      case 'up':
+        this.addUp(units);
+        break;
+    }
+  }
+}
+
+function getFinalPosition(instructions) {
+  const positon = new Position();
+  instructions.forEach((instruction) => {
+    positon.addInstruction(instruction);
+  });
+  return positon;
+}
+
+function getResult(input = getInput()) {
+  const finalPosition = getFinalPosition(input);
+  return finalPosition.x * finalPosition.y;
+}
 
 module.exports = {
+  Position,
+  getFinalPosition,
   getResult,
 };
--- days/02/part-one.test.js
+++ days/02/part-one.test.js
@@ -1,9 +1,90 @@
 const partOne = require('./part-one');
+const { Position } = require('./part-one');
 
 describe('day2 | part-one', () => {
+  describe('Position', () => {
+    let position;
+    beforeEach(() => {
+      position = new Position();
+    });
+
+    describe('#addForward', () => {
+      it('should decrease x', () => {
+        position.addForward(5);
+
+        expect(position.x).toBe(5);
+        expect(position.y).toBe(0);
+      });
+    });
+
+    describe('#addDown', () => {
+      it('should increase y', () => {
+        position.addDown(5);
+
+        expect(position.x).toBe(0);
+        expect(position.y).toBe(5);
+      });
+    });
+
+    describe('#addUp', () => {
+      it('should decrease y', () => {
+        position.addUp(5);
+
+        expect(position.x).toBe(0);
+        expect(position.y).toBe(-5);
+      });
+    });
+
+    describe('#addInstruction', () => {
+      [
+        { instruction: 'forward', units: 5, expectedX: 5, expectedY: 0 },
+        { instruction: 'up', units: 5, expectedX: 0, expectedY: -5 },
+        { instruction: 'down', units: 5, expectedX: 0, expectedY: 5 },
+      ].forEach((testCase) => {
+        it(`should be return x = ${testCase.expectedX} and y = ${testCase.expectedY}`, () => {
+          const position = new Position();
+
+          position.addInstruction(testCase);
+
+          expect(position.x).toBe(testCase.expectedX);
+          expect(position.y).toBe(testCase.expectedY);
+        });
+      });
+    });
+  });
+
+  describe('#getFinalPosition', () => {
+    it('should return the final position', () => {
+      const instructions = [
+        { instruction: 'forward', units: 5 },
+        { instruction: 'down', units: 5 },
+        { instruction: 'forward', units: 8 },
+        { instruction: 'up', units: 3 },
+        { instruction: 'down', units: 8 },
+        { instruction: 'forward', units: 2 },
+      ];
+
+      const result = partOne.getFinalPosition(instructions);
+
+      expect(result.x).toBe(15);
+      expect(result.y).toBe(10);
+    });
+  });
+
   describe('#getResult', () => {
-    it('', () => {
-      partOne.getResult();
+    it('should getFinalPositon and multiplie position and depth', () => {
+      const instructions = [
+        { instruction: 'forward', units: 5 },
+        { instruction: 'down', units: 5 },
+        { instruction: 'forward', units: 8 },
+        { instruction: 'up', units: 3 },
+        { instruction: 'down', units: 8 },
+        { instruction: 'forward', units: 2 },
+      ];
+
+      const result = partOne.getResult(instructions);
+
+      expect(result).toBe(150);
     });
   });
 });
