c6da8d231a8c4ccf8a7d67c175cb7fdf9dc38ee2
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-05 09:30:27 +0100

    day05: part2

--- days/05/part-two.js
+++ days/05/part-two.js
@@ -1,7 +1,78 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.overlap = 1;
+  }
+
+  addOverlap() {
+    this.overlap++;
+  }
+
+  static getAllPointsBetween({ x1, y1, x2, y2 }) {
+    if (x1 === x2) {
+      const range = getRange(y1, y2);
+      return range.map((y) => new Point(x1, y));
+    } else if (y1 === y2) {
+      const range = getRange(x1, x2);
+      return range.map((x) => new Point(x, y1));
+    } else {
+      const xRange = getRange(x1, x2);
+      const yRange = getRange(y1, y2);
+
+      return xRange.map((x, index) => {
+        return new Point(x, yRange[index]);
+      });
+    }
+  }
+}
+
+function getRange(start, end) {
+  if (start < end) {
+    const range = _.range(start, end, 1);
+    range.push(end);
+    return range;
+  }
+
+  const range = _.rangeRight(end, start, 1);
+  range.unshift(start);
+  return range;
+}
+
+class Grid {
+  constructor(lines) {
+    this.points = [];
+    for (let line of lines) {
+      for (let point of line) {
+        this.addPoint(point);
+      }
+    }
+  }
+
+  addPoint(newPoint) {
+    const foundPoint = this.points.find((point) => point.x === newPoint.x && point.y === newPoint.y);
+    if (foundPoint) {
+      foundPoint.addOverlap();
+    } else {
+      this.points.push(newPoint);
+    }
+  }
+
+  dangerousAreas() {
+    return this.points.filter((point) => point.overlap > 1).length;
+  }
+}
+
+function getResult(input = getInput()) {
+  const lines = input.map((line) => Point.getAllPointsBetween(line));
+  const grid = new Grid(lines);
+  return grid.dangerousAreas();
+}
 
 module.exports = {
+  Point,
   getResult,
 };
--- days/05/part-two.test.js
+++ days/05/part-two.test.js
@@ -1,11 +1,82 @@
 const partTwo = require('./part-two');
 
 describe('day5 | part-two', () => {
+  describe('Point', () => {
+    describe('#getAllPointsBetween', () => {
+      describe("when the x's are the same : horizontal line", () => {
+        it('should return all points between', () => {
+          const result = partTwo.Point.getAllPointsBetween({ x1: 1, y1: 1, x2: 1, y2: 3 });
+
+          expect(result.length).toBe(3);
+          expect(result[0].x).toBe(1);
+          expect(result[0].y).toBe(1);
+          expect(result[1].x).toBe(1);
+          expect(result[1].y).toBe(2);
+          expect(result[2].x).toBe(1);
+          expect(result[2].y).toBe(3);
+        });
+      });
+
+      describe("when the y's are the same = vertical line", () => {
+        it('should return all points between ', () => {
+          const result = partTwo.Point.getAllPointsBetween({ x1: 9, y1: 7, x2: 7, y2: 7 });
+
+          expect(result.length).toBe(3);
+          expect(result[0].x).toBe(9);
+          expect(result[0].y).toBe(7);
+          expect(result[1].x).toBe(8);
+          expect(result[1].y).toBe(7);
+          expect(result[2].x).toBe(7);
+          expect(result[2].y).toBe(7);
+        });
+      });
+
+      describe("when the x's and the y's are different = diagonal line", () => {
+        it('should return all points between 1,1-> 3,3', () => {
+          const result = partTwo.Point.getAllPointsBetween({ x1: 1, y1: 1, x2: 3, y2: 3 });
+
+          expect(result.length).toBe(3);
+          expect(result[0].x).toBe(1);
+          expect(result[0].y).toBe(1);
+          expect(result[1].x).toBe(2);
+          expect(result[1].y).toBe(2);
+          expect(result[2].x).toBe(3);
+          expect(result[2].y).toBe(3);
+        });
+
+        it('should return all points between 9,7 -> 7,9', () => {
+          const result = partTwo.Point.getAllPointsBetween({ x1: 9, y1: 7, x2: 7, y2: 9 });
+
+          expect(result.length).toBe(3);
+          expect(result[0].x).toBe(9);
+          expect(result[0].y).toBe(7);
+          expect(result[1].x).toBe(8);
+          expect(result[1].y).toBe(8);
+          expect(result[2].x).toBe(7);
+          expect(result[2].y).toBe(9);
+        });
+      });
+    });
+  });
+
   describe('#getResult', () => {
     it('should ', () => {
-      const result = partTwo.getResult();
+      const input = [
+        { x1: 0, y1: 9, x2: 5, y2: 9 },
+        { x1: 8, y1: 0, x2: 0, y2: 8 },
+        { x1: 9, y1: 4, x2: 3, y2: 4 },
+        { x1: 2, y1: 2, x2: 2, y2: 1 },
+        { x1: 7, y1: 0, x2: 7, y2: 4 },
+        { x1: 6, y1: 4, x2: 2, y2: 0 },
+        { x1: 0, y1: 9, x2: 2, y2: 9 },
+        { x1: 3, y1: 4, x2: 1, y2: 4 },
+        { x1: 0, y1: 0, x2: 8, y2: 8 },
+        { x1: 5, y1: 5, x2: 8, y2: 2 },
+      ];
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(12);
     });
   });
 });
