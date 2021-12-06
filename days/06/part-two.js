506ad22556261c72f2383e74311d8ccc9c6d1528
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-06 10:18:53 +0100

    day06: part2

--- days/06/part-two.js
+++ days/06/part-two.js
@@ -1,7 +1,69 @@
 const { getInput } = require('./input');
 
-function getResult(input = getInput()) {}
+class Lanternfish {
+  constructor(timer) {
+    this.timer = timer;
+  }
+
+  nextDay() {
+    if (this.timer === 0) {
+      this.timer = 6;
+      return new Lanternfish(8);
+    }
+    this.timer--;
+    return null;
+  }
+}
+
+class State {
+  constructor(lanternfishs) {
+    this.state = new Map();
+    lanternfishs.forEach((timer) => {
+      let foundTimer = this.state.get(timer);
+      if (foundTimer) {
+        this.state.set(timer, ++foundTimer);
+      } else {
+        this.state.set(timer, 1);
+      }
+    });
+  }
+
+  nextDay() {
+    const newState = new Map();
+    for (let [timer, number] of this.state) {
+      const lanternfish = new Lanternfish(timer);
+      const newLanternfish = lanternfish.nextDay();
+      const foundTimer = newState.get(lanternfish.timer);
+      if (foundTimer) {
+        newState.set(lanternfish.timer, foundTimer + number);
+      } else {
+        newState.set(lanternfish.timer, number);
+      }
+      if (newLanternfish) {
+        newState.set(newLanternfish.timer, number);
+      }
+    }
+    this.state = newState;
+  }
+
+  length() {
+    let length = 0;
+    for (let number of this.state.values()) {
+      length += number;
+    }
+    return length;
+  }
+}
+
+function getResult(input = getInput()) {
+  const state = new State(input);
+  for (let day = 0; day < 256; day++) {
+    state.nextDay();
+  }
+  return state.length();
+}
 
 module.exports = {
+  Lanternfish,
   getResult,
 };
--- days/06/part-two.test.js
+++ days/06/part-two.test.js
@@ -1,11 +1,37 @@
 const partTwo = require('./part-two');
 
-describe('day6 | part-two', () => {
+describe('day6 | part-one', () => {
+  describe('Lanternfish', () => {
+    describe('#nextDay', () => {
+      it('should decrease internal time', () => {
+        const lanternfish = new partTwo.Lanternfish(3);
+
+        lanternfish.nextDay();
+
+        expect(lanternfish.timer).toBe(2);
+      });
+
+      describe('when time is 0', () => {
+        it('should create new lanternfish and set timer to 6', () => {
+          const lanternfish = new partTwo.Lanternfish(0);
+
+          const result = lanternfish.nextDay();
+
+          expect(lanternfish.timer).toBe(6);
+          expect(result).toBeInstanceOf(partTwo.Lanternfish);
+          expect(result.timer).toBe(8);
+        });
+      });
+    });
+  });
+
   describe('#getResult', () => {
     it('should ', () => {
-      const result = partTwo.getResult();
+      const input = [3, 4, 3, 1, 2];
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(26984457539);
     });
   });
 });
