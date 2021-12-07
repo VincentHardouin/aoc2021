52e736d25b432d76cb1677cab3e801be0af850c7
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-07 13:25:29 +0100

    day03: fix input test

--- days/03/input.test.js
+++ days/03/input.test.js
@@ -3,6 +3,6 @@ const input = require('./input');
 describe('day3 | input', () => {
   it('should return array of int for each line', () => {
     const result = input.getInput();
-    expect(result[0]).toBe(0b00001000101);
+    expect(result[0]).toBe('000001000101');
   });
 });
