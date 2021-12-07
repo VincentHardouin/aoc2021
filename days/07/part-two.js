42dbbb9501abc7acf5b20e0c0bebd3b0c1c6626a
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-07 09:06:29 +0100

    day07: part two

--- days/07/part-two.js
+++ days/07/part-two.js
@@ -1,6 +1,29 @@
 const { getInput } = require('./input');
 
-function getResult(input = getInput()) {}
+function computeTotalFuelCost(positions) {
+  const fuelCosts = [];
+  const max = Math.max(...positions);
+  for (let i = 0; i < max; i++) {
+    let fuelCost = 0;
+    for (let j = 0; j < positions.length; j++) {
+      fuelCost += computeFuelCost(Math.abs(positions[j] - i));
+    }
+    fuelCosts.push(fuelCost);
+  }
+  return Math.min(...fuelCosts);
+}
+
+function computeFuelCost(moves) {
+  let number = 0;
+  for (let i = 1; i <= moves; i++) {
+    number += i;
+  }
+  return number;
+}
+
+function getResult(input = getInput()) {
+  return computeTotalFuelCost(input);
+}
 
 module.exports = {
   getResult,
--- days/07/part-two.test.js
+++ days/07/part-two.test.js
@@ -3,9 +3,11 @@ const partTwo = require('./part-two');
 describe('day07 | part-two', () => {
   describe('#getResult', () => {
     it('should ', () => {
-      const result = partTwo.getResult();
+      const input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(168);
     });
   });
 });
