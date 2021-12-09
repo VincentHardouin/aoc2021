0491b88980bde41423886e054fcf689659102c93
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-09 15:49:02 +0100

    day09: part2

--- days/09/part-two.js
+++ days/09/part-two.js
@@ -1,6 +1,115 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+class Location {
+  constructor(x, y, height, adjacentLocationsHeight) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.adjacentLocationsHeight = adjacentLocationsHeight;
+  }
+
+  isLowPoints() {
+    return this.height < Math.min(...this.adjacentLocationsHeight);
+  }
+}
+
+class Basin {
+  constructor(lowPoint) {
+    this.lowPoint = lowPoint;
+    this.points = [];
+  }
+
+  addPoint(location) {
+    if (!this.contains(location)) {
+      this.points.push(location);
+    }
+  }
+
+  contains(location) {
+    return !!this.points.find((point) => point.x === location.x && point.y === location.y);
+  }
+}
+
+function getLocations(input) {
+  const locations = [];
+  for (let y = 0; y < input.length; y++) {
+    const locationInLine = [];
+    for (let x = 0; x < input[0].length; x++) {
+      const height = input[y][x];
+      const adjacentLocationsHeight = [];
+
+      if (y > 0) {
+        adjacentLocationsHeight.push(input[y - 1][x]);
+      }
+      if (x > 0) {
+        adjacentLocationsHeight.push(input[y][x - 1]);
+      }
+      if (y < input.length - 1) {
+        adjacentLocationsHeight.push(input[y + 1][x]);
+      }
+      if (x < input[0].length - 1) {
+        adjacentLocationsHeight.push(input[y][x + 1]);
+      }
+
+      const location = new Location(x, y, height, adjacentLocationsHeight);
+      locationInLine.push(location);
+    }
+    locations.push(locationInLine);
+  }
+  return locations;
+}
+
+function getAdjacent(location, locations) {
+  const x = location.x;
+  const y = location.y;
+  const adjacentLocations = [];
+
+  if (y > 0) {
+    adjacentLocations.push(locations[y - 1][x]);
+  }
+  if (x > 0) {
+    adjacentLocations.push(locations[y][x - 1]);
+  }
+  if (y < locations.length - 1) {
+    adjacentLocations.push(locations[y + 1][x]);
+  }
+  if (x < locations[0].length - 1) {
+    adjacentLocations.push(locations[y][x + 1]);
+  }
+  return adjacentLocations;
+}
+
+function getBasin(basin, locations) {
+  let current = basin.lowPoint.height;
+  let neighbours = getAdjacent(basin.lowPoint, locations);
+
+  while (neighbours.length !== 0 && current < 8) {
+    current++;
+    let upWardNeighbours = neighbours.filter((neighbour) => neighbour.height < 9 && neighbour.height > current - 1);
+    upWardNeighbours.forEach((neighbour) => {
+      basin.addPoint(neighbour);
+    });
+    neighbours = upWardNeighbours.map((neighbour) => getAdjacent(neighbour, locations)).flat();
+  }
+}
+
+function getResult(input = getInput()) {
+  const locations = getLocations(input);
+  const lowPoints = locations.flat().filter((location) => location.isLowPoints());
+  const basins = lowPoints.map((lowPoint) => new Basin(lowPoint));
+
+  basins.forEach((basin) => {
+    getBasin(basin, locations);
+  });
+
+  return _(basins)
+    .sortBy((basin) => basin.points.length)
+    .takeRight(3)
+    .reduce((acc, basin) => {
+      return acc * (basin.points.length + 1);
+    }, 1);
+}
 
 module.exports = {
   getResult,
--- days/09/part-two.test.js
+++ days/09/part-two.test.js
@@ -2,10 +2,18 @@ const partTwo = require('./part-two');
 
 describe('day09 | part-two', () => {
   describe('#getResult', () => {
-    it('should ', () => {
-      const result = partTwo.getResult();
+    it('should return', () => {
+      const input = [
+        [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
+        [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
+        [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
+        [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
+        [9, 8, 9, 9, 9, 6, 5, 6, 7, 8],
+      ];
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(1134);
     });
   });
 });
