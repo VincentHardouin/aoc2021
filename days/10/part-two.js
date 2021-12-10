54b17e7cd334617758f6c6c665437fd7d5358f76
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-10 10:48:08 +0100

    day10: part2

--- days/10/part-two.js
+++ days/10/part-two.js
@@ -1,7 +1,76 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+class Line {
+  constructor(line) {
+    this.line = line;
+  }
+
+  isCorrupted() {
+    const reduce = this.reduce();
+    const regex = /<}|\[}|\(}|<]|{]|\(]|<\)|{\)|\[\)|\(>|{>|\[>/g;
+    return regex.test(reduce);
+  }
+
+  isIncomplete() {
+    return this.reduce() !== '' && !this.isCorrupted();
+  }
+
+  reduce() {
+    const regex = /<>|\[]|{}|\(\)/g;
+    let reducedLine = this.line;
+    while (this.line !== '' && regex.test(reducedLine)) {
+      reducedLine = reducedLine.replace(regex, '');
+    }
+    return reducedLine;
+  }
+
+  getCompletion() {
+    return this.reduce()
+      .split('')
+      .reverse()
+      .map((char) => {
+        if (char === '{') {
+          return '}';
+        } else if (char === '[') {
+          return ']';
+        } else if (char === '(') {
+          return ')';
+        } else if (char === '<') {
+          return '>';
+        }
+      })
+      .join('');
+  }
+}
+
+function getScore(completion) {
+  return completion
+    .split('')
+    .map((char) => {
+      if (char === ')') {
+        return 1;
+      } else if (char === ']') {
+        return 2;
+      } else if (char === '}') {
+        return 3;
+      } else if (char === '>') {
+        return 4;
+      }
+    })
+    .reduce((acc, value) => {
+      return acc * 5 + value;
+    }, 0);
+}
+
+function getResult(input = getInput()) {
+  const lines = input.map((line) => new Line(line));
+  const completions = lines.filter((line) => line.isIncomplete()).map((line) => line.getCompletion());
+  const scores = completions.map(getScore).sort((a, b) => a - b);
+  return scores[Math.floor(scores.length / 2)];
+}
 
 module.exports = {
+  Line,
   getResult,
 };
--- days/10/part-two.test.js
+++ days/10/part-two.test.js
@@ -1,11 +1,64 @@
 const partTwo = require('./part-two');
 
 describe('day10 | part-two', () => {
+  describe('Line', () => {
+    describe('#isIncomplete', () => {
+      it('should return true when line is not complete', () => {
+        const line = new partTwo.Line('[({(<(())[]>[[{[]{<()<>>');
+
+        expect(line.isIncomplete()).toBe(true);
+      });
+    });
+
+    describe('#isCorrupted', () => {
+      it('should return true when line is corrupted', () => {
+        const line = new partTwo.Line('{([(<{}[<>[]}>{[]{[(<()>');
+
+        expect(line.isCorrupted()).toBe(true);
+      });
+
+      it('should return false when line is not corrupted', () => {
+        const line = new partTwo.Line('{([(<{}[<>[]]>{[]{[(<()>');
+
+        expect(line.isCorrupted()).toBe(false);
+      });
+    });
+
+    describe('#getCompletion', () => {
+      [
+        { line: '[({(<(())[]>[[{[]{<()<>>', completion: '}}]])})]' },
+        { line: '[(()[<>])]({[<{<<[]>>(', completion: ')}>]})' },
+        { line: '(((({<>}<{<{<>}{[]{[]{}', completion: '}}>}>))))' },
+        { line: '{<[[]]>}<{[{[{[]{()[[[]', completion: ']]}}]}]}>' },
+        { line: '<{([{{}}[<[[[<>{}]]]>[]]', completion: '])}>' },
+      ].forEach((testCase) => {
+        it(`should return completion : ${testCase.completion}`, () => {
+          const line = new partTwo.Line(testCase.line);
+
+          expect(line.getCompletion()).toBe(testCase.completion);
+        });
+      });
+    });
+  });
+
   describe('#getResult', () => {
     it('should ', () => {
-      const result = partTwo.getResult();
+      const input = [
+        '[({(<(())[]>[[{[]{<()<>>',
+        '[(()[<>])]({[<{<<[]>>(',
+        '{([(<{}[<>[]}>{[]{[(<()>',
+        '(((({<>}<{<{<>}{[]{[]{}',
+        '[[<[([]))<([[{}[[()]]]',
+        '[{[{({}]{}}([{[{{{}}([]',
+        '{<[[]]>}<{[{[{[]{()[[[]',
+        '[<(<(<(<{}))><([]([]()',
+        '<{([([[(<>()){}]>(<<{{',
+        '<{([{{}}[<[[[<>{}]]]>[]]',
+      ];
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(288957);
     });
   });
 });
