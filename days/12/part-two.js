aee95d6e82d4bb290b7aa24a324373d61deee506
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-12 22:28:50 +0100

    day12: part 2

--- days/12/part-two.js
+++ days/12/part-two.js
@@ -1,7 +1,100 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
+const { value } = require('lodash/seq');
 
-function getResult(input = getInput()) {}
+class Segment {
+  constructor(segment) {
+    this.a = segment[0];
+    this.b = segment[1];
+  }
+
+  contains(value) {
+    return this.a === value || this.b === value;
+  }
+
+  getOtherCave(value) {
+    return value === this.a ? this.b : this.a;
+  }
+
+  getSmallCaves() {
+    const smallCaves = [];
+    const regex = /[a-z]/;
+    if (regex.test(this.a) && this.a !== 'start' && this.a !== 'end') {
+      smallCaves.push(this.a);
+    }
+    if (regex.test(this.b) && this.b !== 'start' && this.b !== 'end') {
+      smallCaves.push(this.b);
+    }
+    return smallCaves;
+  }
+
+  includes(caves) {
+    return [this.a, this.b].some((point) => caves.includes(point));
+  }
+}
+
+class Path {
+  constructor(segments) {
+    this.path = [...segments];
+  }
+
+  getLastCave() {
+    let otherCave = this.path[0].getOtherCave('start');
+    for (let i = 1; i < this.path.length; i++) {
+      otherCave = this.path[i].getOtherCave(otherCave);
+    }
+    return otherCave;
+  }
+
+  getSmallCaves() {
+    const allSmallCaves = this.path.reduce((acc, segment) => {
+      acc.push(...segment.getSmallCaves());
+      return acc;
+    }, []);
+    const caves = _.countBy(allSmallCaves);
+
+    const remainingCaves = [];
+    let visitingTwice = null;
+    for (const [key, value] of Object.entries(caves)) {
+      if (value > 2) {
+        visitingTwice = key;
+        if (value === 4) {
+          remainingCaves.push(key);
+        }
+      } else if (value === 2) {
+        remainingCaves.push(key);
+      }
+    }
+
+    return visitingTwice ? remainingCaves : [];
+  }
+
+  getAllPathPossibilitiesFromSegments(segments) {
+    const lastCave = this.getLastCave();
+    if (lastCave === 'end') {
+      return [this];
+    }
+    const visitedSmallCaves = this.getSmallCaves();
+    const possibilities = segments.filter(
+      (segment) => segment.contains(lastCave) && !segment.includes(visitedSmallCaves)
+    );
+    return possibilities.map((possibility) => new Path([...this.path, possibility]));
+  }
+}
+
+function getResult(input = getInput()) {
+  const segments = input.map((segment) => new Segment(segment));
+  let paths = segments.filter((segment) => segment.contains('start')).map((segment) => new Path([segment]));
+  let remainingSegments = segments.filter((segment) => !segment.contains('start'));
+
+  while (paths.filter((path) => path.getLastCave() === 'end').length !== paths.length) {
+    paths = paths.map((path) => path.getAllPathPossibilitiesFromSegments(remainingSegments)).flat();
+  }
+  return paths.length;
+}
 
 module.exports = {
+  Path,
+  Segment,
   getResult,
 };
--- days/12/part-two.test.js
+++ days/12/part-two.test.js
@@ -1,11 +1,143 @@
 const partTwo = require('./part-two');
 
 describe('day12 | part-two', () => {
-  describe('#getResult', () => {
-    it('should ', () => {
-      const result = partTwo.getResult();
+  describe('Segment', () => {
+    describe('#getSmallCaves', () => {
+      it('should return small cave only if appears twice', () => {
+        const segment = new partTwo.Segment(['start', 'dc']);
 
-      expect(result).toBe();
+        expect(segment.getSmallCaves()).toStrictEqual(['dc']);
+      });
+    });
+  });
+
+  describe('Path', () => {
+    describe('#getLastCave', () => {
+      describe('when they are one segment', () => {
+        it('should not return start segment', () => {
+          const segments = [new partTwo.Segment(['start', 'dc'])];
+          const path = new partTwo.Path(segments);
+
+          const result = path.getLastCave();
+
+          expect(result).toBe('dc');
+        });
+      });
+
+      describe('when they are some segments', () => {
+        it('should return last segment', () => {
+          const segments = [
+            new partTwo.Segment(['start', 'A']),
+            new partTwo.Segment(['A', 'b']),
+            new partTwo.Segment(['b', 'A']),
+            new partTwo.Segment(['A', 'c']),
+            new partTwo.Segment(['A', 'c']),
+          ];
+          const path = new partTwo.Path(segments);
+
+          const result = path.getLastCave();
+
+          expect(result).toBe('A');
+        });
+      });
+    });
+
+    describe('#getSmallCaves', () => {
+      it('should return all small cave only if one appears fourth', () => {
+        const segments = [
+          new partTwo.Segment(['start', 'A']),
+          new partTwo.Segment(['A', 'b']),
+          new partTwo.Segment(['b', 'A']),
+          new partTwo.Segment(['A', 'c']),
+          new partTwo.Segment(['c', 'A']),
+          new partTwo.Segment(['c', 'A']),
+          new partTwo.Segment(['c', 'A']),
+        ];
+        const path = new partTwo.Path(segments);
+
+        const result = path.getSmallCaves();
+
+        expect(result).toStrictEqual(['b', 'c']);
+      });
+
+      it('should not return small cave when any appears fourth', () => {
+        const segments = [
+          new partTwo.Segment(['start', 'A']),
+          new partTwo.Segment(['A', 'b']),
+          new partTwo.Segment(['b', 'A']),
+          new partTwo.Segment(['A', 'c']),
+          new partTwo.Segment(['c', 'A']),
+        ];
+        const path = new partTwo.Path(segments);
+
+        const result = path.getSmallCaves();
+
+        expect(result).toStrictEqual([]);
+      });
+    });
+  });
+
+  describe('#getResult', () => {
+    it('should return the number of path : small exemple', () => {
+      const input = [
+        ['start', 'A'],
+        ['start', 'b'],
+        ['A', 'c'],
+        ['A', 'b'],
+        ['b', 'd'],
+        ['A', 'end'],
+        ['b', 'end'],
+      ];
+
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(36);
+    });
+
+    it('should return the number of path : small exemple2', () => {
+      const input = [
+        ['dc', 'end'],
+        ['HN', 'start'],
+        ['start', 'kj'],
+        ['dc', 'start'],
+        ['dc', 'HN'],
+        ['LN', 'dc'],
+        ['HN', 'end'],
+        ['kj', 'sa'],
+        ['kj', 'HN'],
+        ['kj', 'dc'],
+      ];
+
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(103);
+    });
+
+    it('should return the number of path', () => {
+      const input = [
+        ['fs', 'end'],
+        ['he', 'DX'],
+        ['fs', 'he'],
+        ['start', 'DX'],
+        ['pj', 'DX'],
+        ['end', 'zg'],
+        ['zg', 'sl'],
+        ['zg', 'pj'],
+        ['pj', 'he'],
+        ['RW', 'he'],
+        ['fs', 'DX'],
+        ['pj', 'RW'],
+        ['zg', 'RW'],
+        ['start', 'pj'],
+        ['he', 'WI'],
+        ['zg', 'he'],
+        ['pj', 'fs'],
+        ['start', 'RW'],
+      ];
+
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(3509);
     });
   });
 });
