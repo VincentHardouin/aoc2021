6308e83697d999e2815ef38b06298ed1339a182e
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-13 08:26:07 +0100

    day13: prepare

---
+++ days/13/part-two.test.js
@@ -0,0 +1,11 @@
+const partTwo = require('./part-two');
+
+describe('day13 | part-two', () => {
+  describe('#getResult', () => {
+    it('should ', () => {
+      const result = partTwo.getResult();
+
+      expect(result).toBe();
+    });
+  });
+});
