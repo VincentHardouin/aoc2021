34df7147b74ef4b2c3c9f27e1a58ace06c6b0715
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-14 15:51:18 +0100

    day14: part2

--- days/14/part-two.js
+++ days/14/part-two.js
@@ -1,7 +1,103 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+class Rules {
+  constructor(rules) {
+    this.rules = rules;
+  }
+
+  getPairInsertion(pair) {
+    return this.rules.get(pair);
+  }
+}
+
+class Polymer {
+  constructor(polymer, rules, number) {
+    this.polymer = polymer;
+    this.rules = rules;
+    this.number = number;
+  }
+
+  getPairs() {
+    const polymer = this.polymer.split('');
+    const pairs = [];
+    for (let i = 0; i < polymer.length - 1; i++) {
+      const pair = `${polymer[i]}${polymer[i + 1]}`;
+      pairs.push(pair);
+    }
+    return pairs;
+  }
+
+  nextStep() {
+    const pairs = this.getPairs();
+    const newElements = [];
+    for (let i = 0; i < pairs.length; i++) {
+      let elements = pairs[i].charAt(0) + this.rules.getPairInsertion(pairs[i]);
+      newElements.push(elements);
+    }
+    newElements.push(pairs[pairs.length - 1].charAt(1));
+    this.polymer = newElements.join('');
+  }
+
+  getAnalytics() {
+    const elements = this.polymer.split('');
+    const analytics = _.countBy(elements);
+    for (const [key, value] of Object.entries(analytics)) {
+      analytics[key] = value * this.number;
+    }
+    return analytics;
+  }
+}
+
+function getMinAndMax(object) {
+  const minKey = Object.keys(object).reduce((a, b) => (object[a] < object[b] ? a : b));
+  const maxKey = Object.keys(object).reduce((a, b) => (object[a] > object[b] ? a : b));
+  return {
+    min: object[minKey],
+    max: object[maxKey],
+  };
+}
+
+function getResult(input = getInput()) {
+  const rules = new Rules(input.rules);
+  let polymers = new Map();
+  polymers.set(input.polymer, new Polymer(input.polymer, rules, 1));
+
+  for (let i = 0; i < 40; i++) {
+    const newPolymers = new Map();
+    polymers.forEach((polymer) => {
+      polymer.nextStep();
+      const pairs = polymer.getPairs();
+      pairs.forEach((pair) => {
+        let number = polymer.number;
+        if (newPolymers.has(pair)) {
+          number += newPolymers.get(pair).number;
+        }
+        newPolymers.set(pair, new Polymer(pair, rules, number));
+      });
+    });
+    polymers = newPolymers;
+  }
+
+  const analytics = [...polymers.values()]
+    .map((polymer) => polymer.getAnalytics())
+    .reduce((acc, analytic) => {
+      for (const [key, value] of Object.entries(analytic)) {
+        if (acc[key]) {
+          acc[key] += value;
+        } else {
+          acc[key] = value;
+        }
+      }
+      return acc;
+    }, {});
+
+  const { min, max } = getMinAndMax(analytics);
+  return Math.ceil((max - min) / 2);
+}
 
 module.exports = {
+  Rules,
+  Polymer,
   getResult,
 };
--- days/14/part-two.test.js
+++ days/14/part-two.test.js
@@ -1,11 +1,88 @@
 const partTwo = require('./part-two');
 
 describe('day14 | part-two', () => {
+  describe('Rules', () => {
+    describe('#getPairInsertion', () => {
+      it('should return elements', () => {
+        const rulesInput = new Map();
+        rulesInput.set('CH', 'B');
+        const rules = new partTwo.Rules(rulesInput);
+
+        const result = rules.getPairInsertion('CH');
+
+        expect(result).toStrictEqual('B');
+      });
+    });
+  });
+
+  describe('Polymer', () => {
+    describe('#getPairs', () => {
+      it('should return pairs', () => {
+        const polymer = new partTwo.Polymer('NNCB', '');
+
+        const result = polymer.getPairs();
+
+        expect(result).toStrictEqual(['NN', 'NC', 'CB']);
+      });
+    });
+
+    describe('#nextStep', () => {
+      it('should edit polymer', () => {
+        const rulesInput = new Map();
+        rulesInput.set('CH', 'B');
+        rulesInput.set('HH', 'N');
+        rulesInput.set('CB', 'H');
+        rulesInput.set('NH', 'C');
+        rulesInput.set('HB', 'C');
+        rulesInput.set('HC', 'B');
+        rulesInput.set('HN', 'C');
+        rulesInput.set('NN', 'C');
+        rulesInput.set('BH', 'H');
+        rulesInput.set('NC', 'B');
+        rulesInput.set('NB', 'B');
+        rulesInput.set('BN', 'B');
+        rulesInput.set('BB', 'N');
+        rulesInput.set('BC', 'B');
+        rulesInput.set('CC', 'N');
+        rulesInput.set('CN', 'C');
+        const rules = new partTwo.Rules(rulesInput);
+
+        const polymer = new partTwo.Polymer('NNCB', rules);
+
+        polymer.nextStep();
+
+        expect(polymer.polymer).toStrictEqual('NCNBCHB');
+      });
+    });
+  });
+
   describe('#getResult', () => {
     it('should ', () => {
-      const result = partTwo.getResult();
+      const rules = new Map();
+      rules.set('CH', 'B');
+      rules.set('HH', 'N');
+      rules.set('CB', 'H');
+      rules.set('NH', 'C');
+      rules.set('HB', 'C');
+      rules.set('HC', 'B');
+      rules.set('HN', 'C');
+      rules.set('NN', 'C');
+      rules.set('BH', 'H');
+      rules.set('NC', 'B');
+      rules.set('NB', 'B');
+      rules.set('BN', 'B');
+      rules.set('BB', 'N');
+      rules.set('BC', 'B');
+      rules.set('CC', 'N');
+      rules.set('CN', 'C');
+      const input = {
+        polymer: 'NNCB',
+        rules,
+      };
 
-      expect(result).toBe();
+      const result = partTwo.getResult(input);
+
+      expect(result).toBe(2188189693529);
     });
   });
 });
