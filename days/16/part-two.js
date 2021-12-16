3320044e1cefbb32305ed30b3ecbebcb3c4f40c4
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-16 23:16:11 +0100

    day16: part2

--- days/16/part-two.js
+++ days/16/part-two.js
@@ -1,7 +1,107 @@
 const { getInput } = require('./input');
+const _ = require('lodash');
 
-function getResult(input = getInput()) {}
+class Packet {
+  constructor({ version, typeId, lengthTypeId, number, subPackets }) {
+    this.version = parseInt(version, 2);
+    this.typeId = parseInt(typeId, 2);
+    this.lengthTypeId = lengthTypeId;
+    this.number = parseInt(number, 2);
+    this.subPackets = subPackets ?? [];
+  }
+
+  get value() {
+    switch (this.typeId) {
+      case 0:
+        return this.subPackets.reduce((acc, packet) => {
+          return acc + packet.value;
+        }, 0);
+      case 1:
+        return this.subPackets.reduce((acc, packet) => {
+          return acc * packet.value;
+        }, 1);
+      case 2:
+        return _.minBy(this.subPackets, 'value').value;
+      case 3:
+        return _.maxBy(this.subPackets, 'value').value;
+      case 4:
+        return this.number;
+      case 5:
+        return this.subPackets[0].value > this.subPackets[1].value ? 1 : 0;
+      case 6:
+        return this.subPackets[0].value < this.subPackets[1].value ? 1 : 0;
+      case 7:
+        return this.subPackets[0].value === this.subPackets[1].value ? 1 : 0;
+    }
+  }
+
+  static getPacketFromArray(array) {
+    const version = array.splice(0, 3).join('');
+    const typeId = array.splice(0, 3).join('');
+
+    if (typeId === '100') {
+      let number = '';
+      let nextBit = '';
+      while (nextBit !== '0') {
+        nextBit = array.shift();
+        let nextNumber = array.splice(0, 4);
+        number += nextNumber.join('');
+      }
+      return new Packet({ version, typeId, number });
+    }
+
+    const lengthTypeId = array.shift();
+
+    if (lengthTypeId == '0') {
+      const totalSubPacketsLength = parseInt(array.splice(0, 15).join(''), 2);
+      const subPacketsBinary = array.splice(0, totalSubPacketsLength);
+      const subPackets = [];
+      let previousLength = -1;
+      while (subPacketsBinary.length !== 0 && subPackets.length > previousLength) {
+        previousLength = subPackets.length;
+        const subPacket = Packet.getPacketFromArray(subPacketsBinary);
+        if (subPacket) subPackets.push(subPacket);
+      }
+      return new Packet({ version, typeId, lengthTypeId, subPackets });
+    }
+
+    if (lengthTypeId == '1') {
+      const numberOfSubPackets = parseInt(array.splice(0, 11).join(''), 2);
+
+      const subPackets = [];
+      for (let i = 0; i < numberOfSubPackets; i++) {
+        const subPacket = Packet.getPacketFromArray(array);
+        if (subPacket) subPackets.push(subPacket);
+      }
+
+      return new Packet({ version, typeId, lengthTypeId, subPackets });
+    }
+
+    return null;
+  }
+}
+
+function getResult(input = getInput()) {
+  const binary = input
+    .split('')
+    .map((value) => parseInt(value, 16).toString(2).padStart(4, '0').split(''))
+    .flat();
+
+  const packets = [];
+  let previous = -1;
+  while (packets.length > previous) {
+    previous = packets.length;
+    const packet = Packet.getPacketFromArray(binary);
+    if (packet) packets.push(packet);
+  }
+
+  return packets.reduce((acc, packet) => {
+    return acc + packet.value;
+  }, 0);
+}
 
 module.exports = {
+  Packet,
   getResult,
 };
+0;
--- days/16/part-two.test.js
+++ days/16/part-two.test.js
@@ -1,11 +1,101 @@
 const partTwo = require('./part-two');
 
 describe('day16 | part-two', () => {
-  describe('#getResult', () => {
-    it('should ', () => {
-      const result = partTwo.getResult();
+  function hexToArrayOfBit(hex) {
+    return hex
+      .split('')
+      .map((value) => parseInt(value, 16).toString(2).padStart(4, '0').split(''))
+      .flat();
+  }
 
-      expect(result).toBe();
+  describe('Packet', () => {
+    describe('#getPacketFromArray', () => {
+      describe('when it is a literal value', () => {
+        it('should return a Packet when it is a literal value', () => {
+          const array = hexToArrayOfBit('D2FE28');
+
+          const packet = partTwo.Packet.getPacketFromArray(array);
+
+          expect(packet.version).toBe(6);
+          expect(packet.typeId).toBe(4);
+          expect(packet.number).toBe('011111100101');
+        });
+      });
+
+      describe('when it is an operator', () => {
+        describe('when length type == 0', () => {
+          it('should return a Packet', () => {
+            const array = hexToArrayOfBit('38006F45291200');
+
+            const packet = partTwo.Packet.getPacketFromArray(array);
+
+            expect(packet.version).toBe(1);
+            expect(packet.typeId).toBe(6);
+            expect(packet.subPackets.length).toBe(2);
+          });
+        });
+
+        describe('when length type == 1', () => {
+          it('should return a Packet', () => {
+            const array = hexToArrayOfBit('EE00D40C823060');
+
+            const packet = partTwo.Packet.getPacketFromArray(array);
+
+            expect(packet.version).toBe(7);
+            expect(packet.typeId).toBe(3);
+            expect(packet.lengthTypeId).toBe('1');
+            expect(packet.subPackets.length).toBe(3);
+          });
+        });
+      });
+    });
+
+    describe('#value', () => {
+      [
+        { hex: 'C200B40A82', expectedResult: 3 },
+        { hex: '04005AC33890', expectedResult: 54 },
+        { hex: '880086C3E88112', expectedResult: 7 },
+        { hex: 'CE00C43D881120', expectedResult: 9 },
+        { hex: 'D8005AC2A8F0', expectedResult: 1 },
+        { hex: 'F600BC2D8F', expectedResult: 0 },
+        { hex: '9C005AC2F8F0', expectedResult: 0 },
+        { hex: '9C0141080250320F1802104A08', expectedResult: 1 },
+      ].forEach((testCase) => {
+        it(`should return ${testCase.expectedResult}`, () => {
+          const array = hexToArrayOfBit(testCase.hex);
+
+          const packet = partTwo.Packet.getPacketFromArray(array);
+
+          expect(packet.value).toBe(testCase.expectedResult);
+        });
+      });
+    });
+  });
+
+  describe('#getResult', () => {
+    [
+      {
+        input: '8A004A801A8002F478',
+        expectedResult: 16,
+      },
+      {
+        input: '620080001611562C8802118E34',
+        expectedResult: 12,
+      },
+      {
+        input: 'C0015000016115A2E0802F182340',
+        expectedResult: 23,
+      },
+      {
+        input: 'A0016C880162017C3686B18A3D4780',
+        expectedResult: 31,
+      },
+    ].forEach((testCase) => {
+      it(`should return ${testCase.expectedResult}`, () => {
+        const result = partTwo.getResult(testCase.input);
+
+        expect(result).toBe(testCase.expectedResult);
+      });
     });
   });
 });
