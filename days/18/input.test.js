4824a93ee294884d0a7f19fb6bd1ed7a4dd6a2b1
Vincent Hardouin <vincenthardouin@icloud.com>
2021-12-19 20:58:48 +0100

    day18: prepare

---
+++ days/18/input.test.js
@@ -0,0 +1,9 @@
+const input = require('./input');
+
+describe('day18 | input', () => {
+  it('should return array of int for each line', () => {
+    const result = input.getInput();
+
+    expect(result).toBe('');
+  });
+});
